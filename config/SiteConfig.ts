6f5482a15a115ec2ebdc7033ed74dff226cd2df6
Devin Ward <devinw1325wc@gmail.com>
2019-04-07 14:47:36 -0500

    wokring on og meta data

--- config/SiteConfig.ts
+++ config/SiteConfig.ts
@@ -5,7 +5,7 @@ export default {
   siteTitleAlt: 'Thoughts and Blogs from Devin Ward', // Alternative Site title for SEO
   siteUrl: 'http://devsquared.space', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteBanner: '/assets/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  siteBanner: '/assets/banner.png', // Your image for og:image tag. You can find it in the /static folder
   defaultBg: '/assets/bg.png', // default post background header
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Landing Page and Blog Site for Devin Ward', // Your site description
@@ -14,8 +14,13 @@ export default {
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@devissquared', // Twitter Username - Optional
-  //ogSiteName: 'mhadaily', // Facebook Site Name - Optional
-  //ogLanguage: 'en_US', // Facebook Language
+
+  // og meta objects
+  // ogUrl: 'https://devsquared.space/blog/',
+  // ogType: 'article',
+  // ogTitle: 'Blogs from Devin Ward',
+  // ogDesc: 'Check out my blogs!',
+  // ogImage: 'src/favicon.png',
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
